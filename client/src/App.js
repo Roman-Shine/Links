966c5a7b884bc16998dc6d50ed11b0aad4de00e3
rshaynurov <rshaynurov@c-i-p.ru>
2020-01-13 16:05:37 +0300

    master Links and Link page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { useRoutes } from "./routes";
 import { useAuth } from './hooks/auth.hook';
 import { AuthContext } from './context/AuthContext';
 import { Navbar } from './components/Navbar';
+import { Loader } from './components/Loader';
 import 'materialize-css';
 
 function App() {
